ba2c56bc6333236e0077a255e75d424cc8536cbd
Luiz Carlos <lucachaves@gmail.com>
2024-07-08 12:22:31 -0300

    feat: invest-app - create investment, cors

--- projects/invest-app/back-end/src/index.js
+++ projects/invest-app/back-end/src/index.js
@@ -1,13 +1,43 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import { v4 as uuidv4 } from 'uuid';
 import { investments } from './data.js';
 
 const server = express();
 
+server.use(express.json());
+
 server.use(morgan('tiny'));
+
+server.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+  })
+);
 
 server.get('/investments', (req, res) => {
   return res.json(investments);
+});
+
+server.post('/investments', (req, res) => {
+  const investment = req.body;
+
+  const id = uuidv4();
+
+  const newInvestment = { ...investment, id };
+
+  if (investment) {
+    investments.push(newInvestment);
+
+    res.status(201).json(newInvestment);
+  } else {
+    throw new HTTPError('Unable to create investment', 400);
+  }
 });
 
 server.use((req, res, next) => {
